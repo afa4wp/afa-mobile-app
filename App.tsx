47018342bb455f1e648fc768878a5afe30f618ae
claudionhangapc <claudionhangapc@gmail.com>
2023-05-06 19:24:51 -0300

    extended theme from native-base

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { NativeBaseProvider } from "native-base";
 import Routes from './src/routes/index'
 import { NavigationContainer } from '@react-navigation/native';
+import { theme } from "./src/constants/theme"
 
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme = {theme}>
       <NavigationContainer >
         <Routes/>
       </NavigationContainer>
---
+++ src/constants/theme.ts
@@ -0,0 +1,35 @@
+import { extendTheme, ITheme } from "native-base";
+
+const newColorTheme = {
+    brand:{
+      900: '#8287af',
+      800: '#7c83db',
+      700: '#b3bef6',
+    },
+    mark:{
+      900: '#CC3466',
+      800: '#383042',
+      700: "#fff",
+      600: '#CC3466',
+      500: '#383042',
+      400: "#fff",
+      300: '#CC3466',
+      200: '#383042',
+      100: "#fff",
+      50: '#CC3466',
+    },
+    schemahelper:{
+      900: '#0c4a6e',
+      800: '#075985',
+      700: "#0369a1",
+      600: '#383042',
+      500: '#0ea5e9',
+      400: '#38bdf8',
+      300: '#7dd3fc',
+      200: '#bae6fd',
+      100: '#e0f2fe',
+      50: '#f0f9ff',
+    }
+  }
+  
+export const theme: ITheme = extendTheme({colors: newColorTheme});
