f914efc3118e9e654fcb05b660d63b8ebdf9f30b
claudionhangapc <claudionhangapc@gmail.com>
2023-05-07 22:56:47 -0300

    request acess if necessary

--- src/screens/public/signin/SignInQRCodeScreen.tsx
+++ src/screens/public/signin/SignInQRCodeScreen.tsx
@@ -3,11 +3,14 @@ import { Box, VStack, Center, Button, Icon, Text } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignInQRCodeScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
+  const navigation = useNavigation();
+  
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -20,17 +23,74 @@ export function SignInQRCodeScreen() {
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    //navigation.navigate('SignInUserInfo');
   };
 
+  const scanner = () =>{
+    if(hasPermission === false){
+      (async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      })();
+    }
+  }
+
+  if (hasPermission === null) {
+    return (
+      <Box flex={1} my="10" px="5">
+        <Text 
+              color="mark.800"
+              fontSize="md"
+              bold
+              textAlign="center"
+              >
+              Requesting for camera permission
+        </Text>
+      </Box>
+      );
+  }
+
+  if (hasPermission === false) {
+    return (
+        <Box flex={1} bg="mark.700" >
+          <Box flex={1} my="5" px="5">
+            <VStack my="5">
+              <Center>
+                <Text 
+                  color="mark.800"
+                  fontSize="md"
+                  bold
+                  textAlign="center"
+                  >
+                  No access to camera
+                </Text>
+              </Center>
+            </VStack>
+            <VStack>
+              <Button 
+                size="lg"
+                bg="mark.900"
+                leftIcon={<Icon as={Ionicons} name="camera" size="md" />}
+                onPress={() => {
+                  scanner()
+                }}
+                >
+                    Request Acess
+                </Button>
+            </VStack>
+          </Box>  
+        </Box>
+      );
+  }
   return (
     <Box flex={1} bg="mark.700" >
-      <Box flex={1} mt="5" px="5">
+      <Box flex={1} my="5" px="5">
         <VStack flex={1} borderRadius="md" bg="mark.900">
           <BarCodeScanner
             onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
+            style={[StyleSheet.absoluteFillObject]}
           />
-        </VStack>
+        </VStack> 
         <VStack my="5">
           <Center>
             <Text 
@@ -48,6 +108,9 @@ export function SignInQRCodeScreen() {
             size="lg"
             bg="mark.900"
             leftIcon={<Icon as={Ionicons} name="qr-code" size="md" />}
+            onPress={() => {
+              setScanned(false)
+            }}
             >
                 Scan
             </Button>
