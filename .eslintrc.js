902704314019f224ea14bc786ba950181c902fcf
claudionhangapc <claudionhangapc@gmail.com>
2023-05-10 20:52:53 -0300

    added eslint standard

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    rules: {
+      // Your custom rules
+    },
+  };
