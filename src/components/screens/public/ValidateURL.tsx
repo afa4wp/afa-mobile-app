ce587daf20ef97051d0e8372bd1da32a96aaac9c
claudionhangapc <claudionhangapc@gmail.com>
2023-05-11 22:31:41 -0300

    added modal on ValidateURL processing

--- src/components/screens/public/ValidateURL.tsx
+++ src/components/screens/public/ValidateURL.tsx
@@ -9,10 +9,16 @@ import {
   InputLeftAddon,
   Stack,
   FormControl,
+  Modal,
+  HStack,
+  Spinner,
+  Heading,
 } from 'native-base';
 import { Platform } from 'react-native';
+import { useState } from 'react';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
+import axios from 'axios';
 
 const validationSchema = yup.object().shape({
   url: yup
@@ -25,15 +31,35 @@ const validationSchema = yup.object().shape({
 });
 
 export function ValidateURL() {
+  const [showModal, setShowModal] = useState(true);
+
   const formik = useFormik({
     initialValues: {
       url: '',
     },
     validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(values);
+    onSubmit: async (values) => {
+      await checkWordPress('https://claudionhangapc.com/multsite_claudio');
+      //console.log(values);
     },
   });
+
+  async function checkWordPress(url) {
+    const rsdEndpoint = url + '/xmlrpc.php?rsd';
+    console.log('entrou');
+    try {
+      const response = await axios.get(rsdEndpoint);
+      console.log(response);
+      // Check if the response contains the expected RSD structure for WordPress
+      if (response.data.includes('WordPress')) {
+        console.log('WordPress site detected!');
+      } else {
+        console.log('Not a WordPress site.');
+      }
+    } catch (error) {
+      console.log('Error occurred:', error.message);
+    }
+  }
 
   return (
     <Box px="5" flex={1}>
@@ -82,6 +108,20 @@ export function ValidateURL() {
           </Center>
         </VStack>
       </VStack>
+      <Modal isOpen={showModal} size="full" px="5">
+        <Modal.Content maxWidth="100%">
+          <Modal.Body px="0">
+            <HStack space={2} px="2" py="2" alignItems="center">
+              <Spinner
+                size="lg"
+                accessibilityLabel="Check plugin"
+                color="mark.800"
+              />
+              <Text>Verificando o endereço do site</Text>
+            </HStack>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </Box>
   );
 }
