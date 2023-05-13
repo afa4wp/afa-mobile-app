be608bfcb5bb4c90bcfe1c434c4fc122ceba12aa
claudionhangapc <claudionhangapc@gmail.com>
2023-05-13 00:04:20 -0300

    add modal to show login request

--- src/screens/public/signin/SignInCredentialsScreen.tsx
+++ src/screens/public/signin/SignInCredentialsScreen.tsx
@@ -1,11 +1,29 @@
-import { Box, KeyboardAvoidingView } from 'native-base';
+import { Box, KeyboardAvoidingView, Text } from 'native-base';
 import { ValidateURL } from '../../../components/screens/public/ValidateURL';
+import { SignInCredentials } from '../../../components/screens/public/SignInCredentials';
+import { useState } from 'react';
 
 export function SignInCredentialsScreen() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [url, setUrl] = useState('');
+
+  const hendleData = (data: { url: string; status: boolean }) => {
+    if (true === data.status) {
+      setShowLogin(true);
+    } else {
+      setShowLogin(false);
+    }
+    setUrl(data.url);
+  };
+
   return (
     <Box flex={1} bg="mark.700">
       <KeyboardAvoidingView flex={1}>
-        <ValidateURL />
+        {showLogin ? (
+          <SignInCredentials />
+        ) : (
+          <ValidateURL onData={hendleData} />
+        )}
       </KeyboardAvoidingView>
     </Box>
   );
