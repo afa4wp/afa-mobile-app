5f4a406620c95dca79f51969df496120f0fe152e
claudionhangapc <claudionhangapc@gmail.com>
2023-05-13 19:28:33 -0300

    render AppProviders

--- App.tsx
+++ App.tsx
@@ -1,14 +1,5 @@
-import { NativeBaseProvider } from "native-base";
-import Routes from './src/routes/index'
-import { NavigationContainer } from '@react-navigation/native';
-import { theme } from "./src/constants/theme"
+import AppProviders from './src/providers/Providers';
 
 export default function App() {
-  return (
-    <NativeBaseProvider theme = {theme}>
-      <NavigationContainer >
-        <Routes/>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  );
+  return <AppProviders />;
 }
