b61c0d81b9637ab5f45505e9300039e17993a366
claudionhangapc <claudionhangapc@gmail.com>
2023-05-14 02:09:41 -0300

    handle with token

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -2,13 +2,15 @@ import { NativeBaseProvider } from 'native-base';
 import Routes from '../routes/index';
 import { NavigationContainer } from '@react-navigation/native';
 import { theme } from '../constants/theme';
-
+import AuthProvider from './AuthProvider';
 export default function AppProviders() {
   return (
     <NativeBaseProvider theme={theme}>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </AuthProvider>
     </NativeBaseProvider>
   );
 }
