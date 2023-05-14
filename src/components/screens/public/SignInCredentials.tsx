0224dbfe8708aefe492a756a34a7b966ebb85d4e
claudionhangapc <claudionhangapc@gmail.com>
2023-05-14 04:29:31 -0300

    handle login

--- src/components/screens/public/SignInCredentials.tsx
+++ src/components/screens/public/SignInCredentials.tsx
@@ -7,12 +7,23 @@ import {
   Input,
   Pressable,
   Button,
+  Modal,
+  HStack,
+  Spinner,
+  Alert,
+  Collapse,
+  IconButton,
+  CloseIcon,
 } from 'native-base';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
+import AuthContext from '../../../context/AuthContext';
+import * as authService from '../../../services/auth';
+import { LoginData } from '../../../@types/AuthTypes';
+import axios from 'axios';
 
 const validationSchema = yup.object().shape({
   username: yup.string().required('Username is required'),
@@ -20,7 +31,13 @@ const validationSchema = yup.object().shape({
 });
 
 export function SignInCredentials() {
+  const { handleLogin } = useContext(AuthContext);
   const [show, setShow] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [url, setUrl] = useState(
+    'https://claudionhangapc.com/multsite_claudio/wp-json/wp-forms-rest-api/v1'
+  );
 
   const formik = useFormik({
     initialValues: {
@@ -29,9 +46,42 @@ export function SignInCredentials() {
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
-      alert('olaaaaa');
+      await signIn(values);
     },
   });
+
+  const signIn = async (values: LoginData) => {
+    try {
+      setShowModal(true);
+      setShowAlert(false);
+      const result = await authService.signIn(url, values);
+      const { access_token, refresh_token, user_email } = result;
+      handleLogin(access_token, refresh_token, url, user_email);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const response = error.response;
+        if (response) {
+          const { code, message } = response.data;
+          if ('invalid_username' === code) {
+            formik.setErrors({
+              username: message.replace(/<[^>]+>/g, ''), // Assign the error message to the specific field's error
+            });
+          } else if ('incorrect_password' === code) {
+            formik.setErrors({
+              password: message.replace(/<[^>]+>/g, ''), // Assign the error message to the specific field's error
+            });
+          } else {
+            setShowAlert(true);
+          }
+        } else {
+          setShowAlert(true);
+        }
+      } else {
+      }
+    } finally {
+      setShowModal(false);
+    }
+  };
 
   return (
     <Box px="5" flex={1}>
@@ -41,7 +91,9 @@ export function SignInCredentials() {
         </VStack>
         <VStack>
           <FormControl
-            isInvalid={formik.touched.username && formik.errors.username}
+            isInvalid={
+              formik.touched.username && formik.errors.username ? true : false
+            }
           >
             <FormControl.Label
               _text={{
@@ -73,7 +125,9 @@ export function SignInCredentials() {
           </FormControl>
           <FormControl
             mt="3"
-            isInvalid={formik.touched.password && formik.errors.password}
+            isInvalid={
+              formik.touched.password && formik.errors.password ? true : false
+            }
           >
             <FormControl.Label
               _text={{
@@ -115,6 +169,51 @@ export function SignInCredentials() {
             )}
           </FormControl>
         </VStack>
+        <Box w="100%" alignItems="center" mt={showAlert ? '7' : undefined}>
+          <Collapse isOpen={showAlert}>
+            {showAlert && (
+              <Alert maxW="400" status="error">
+                <VStack space={1} flexShrink={1} w="100%">
+                  <HStack
+                    flexShrink={1}
+                    space={2}
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <HStack flexShrink={1} space={2} alignItems="center">
+                      <Alert.Icon />
+                      <Text
+                        fontSize="md"
+                        fontWeight="medium"
+                        _dark={{
+                          color: 'coolGray.800',
+                        }}
+                      >
+                        Error!
+                      </Text>
+                    </HStack>
+                    <IconButton
+                      variant="unstyled"
+                      _focus={{
+                        borderWidth: 0,
+                      }}
+                      icon={<CloseIcon size="3" />}
+                      _icon={{
+                        color: 'coolGray.600',
+                      }}
+                      onPress={() => setShowAlert(false)}
+                    />
+                  </HStack>
+                  <Box w="100%">
+                    <Text color="coolGray.600" fontSize="md">
+                      An error occurred. Please try again later.
+                    </Text>
+                  </Box>
+                </VStack>
+              </Alert>
+            )}
+          </Collapse>
+        </Box>
         <VStack py="7">
           <Button
             size="lg"
@@ -127,6 +226,20 @@ export function SignInCredentials() {
           </Button>
         </VStack>
       </VStack>
+      <Modal isOpen={showModal} size="full" px="5">
+        <Modal.Content maxWidth="100%">
+          <Modal.Body px="0">
+            <HStack space={2} px="2" py="4" alignItems="center">
+              <Spinner
+                size="lg"
+                accessibilityLabel="Check plugin"
+                color="mark.800"
+              />
+              <Text>Fazendo Login</Text>
+            </HStack>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </Box>
   );
 }
