549f2641979dcb9800f589b9c8a47379b1f9f660
claudionhangapc <claudionhangapc@gmail.com>
2023-05-22 01:21:55 -0300

    added items to ConfigurationScreen

--- src/screens/private/configuration/ConfigurationScreen.tsx
+++ src/screens/private/configuration/ConfigurationScreen.tsx
@@ -1,8 +1,30 @@
-
-import { Box } from "native-base";
-
+import { Box } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import { ConfigurationItem } from '../../../components/private/configuration/ConfigurationItem';
+import { Logout } from '../../../components/private/configuration/Logout';
 export function ConfigurationScreen() {
+  const navigation = useNavigation();
+
   return (
-    <Box safeArea  flex={1} >Hello ConfigurationScreen</Box>
+    <Box flex={1} bg="mark.700">
+      <Box px="5" flex={1} mt="2">
+        <ConfigurationItem
+          label="Notificações"
+          iconName="bell"
+          onPress={() => navigation.navigate('NotificationConfig')}
+        />
+        <ConfigurationItem
+          label="Informaçoes pessoais"
+          iconName="user"
+          onPress={() => navigation.navigate('Profile')}
+        />
+        <ConfigurationItem
+          label="Sobre WP AFA"
+          iconName="info-circle"
+          onPress={() => navigation.navigate('About')}
+        />
+        <Logout />
+      </Box>
+    </Box>
   );
 }
