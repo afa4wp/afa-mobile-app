6675a9659eeede18735c686b12412ea5628c917f
claudionhangapc <claudionhangapc@gmail.com>
2023-05-26 14:26:54 -0300

    call NotificationConfigItem

--- src/screens/private/configuration/NotificationConfigScreen.tsx
+++ src/screens/private/configuration/NotificationConfigScreen.tsx
@@ -1,8 +1,22 @@
-
-import { Box } from "native-base";
-
+import { Box, ScrollView, VStack, Heading } from 'native-base';
+import { NotificationConfigItem } from '../../../components/screens/private/configuration/NotificationConfigItem';
 export function NotificationConfigScreen() {
   return (
-    <Box safeArea  flex={1} >Hello NotificationConfigScreen</Box>
+    <Box safeArea flex={1} px="5" bg="mark.700">
+      <ScrollView>
+        <VStack mb="5">
+          <Heading fontSize="xl" color="mark.800">
+            Notificações de envio de formulários
+          </Heading>
+          <NotificationConfigItem label="Por push" isChecked={false} />
+        </VStack>
+        <VStack>
+          <Heading fontSize="xl" color="mark.800">
+            Notificações criação formulários
+          </Heading>
+          <NotificationConfigItem label="Por push" isChecked={false} />
+        </VStack>
+      </ScrollView>
+    </Box>
   );
 }
