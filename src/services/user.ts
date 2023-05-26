8b27e279d21719210833b5671d6126e502440175
claudionhangapc <claudionhangapc@gmail.com>
2023-05-26 01:26:05 -0300

    added method to fetch user info

--- src/services/user.ts
+++ src/services/user.ts
@@ -4,3 +4,8 @@ export const me = async () => {
   let res = await makeApiRequest(`/user/me`);
   return res.data;
 };
+
+export const form_type_me = async (form_type: string) => {
+  let res = await makeApiRequest(`/user/${form_type}/me`);
+  return res.data;
+};
