b7fb06f5206b51376d7ae6674c6c1490707a0a5a
claudionhangapc <claudionhangapc@gmail.com>
2023-05-27 13:19:33 -0300

    created language item component

---
+++ src/components/screens/private/configuration/LanguageItem.tsx
@@ -0,0 +1,27 @@
+import { Pressable, Text, Divider, VStack } from 'native-base';
+import { LanguageItemType } from '../../../../@types/LanguageTypes';
+export function LanguageItem({
+  label,
+  originalLabel,
+  onPress,
+}: LanguageItemType) {
+  return (
+    <VStack>
+      <Pressable onPress={onPress}>
+        <VStack py="4" px="4">
+          <VStack>
+            <Text fontSize="md" color="mark.800">
+              {label}
+            </Text>
+          </VStack>
+          <VStack>
+            <Text fontSize="sm" color="mark.800">
+              {originalLabel}
+            </Text>
+          </VStack>
+        </VStack>
+        <Divider bg="rgba(124, 131, 219, 0.1)" />
+      </Pressable>
+    </VStack>
+  );
+}
