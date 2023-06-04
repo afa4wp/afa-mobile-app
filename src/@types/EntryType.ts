ceba8a04e27847601dfb760c3984fbac54549842
claudionhangapc <claudionhangapc@gmail.com>
2023-06-04 00:56:27 -0300

    created entry type

---
+++ src/@types/EntryType.ts
@@ -0,0 +1,26 @@
+import { User } from './AuthTypes';
+
+type AuthorInfo = {
+  user_name: string;
+  user_email: string;
+  avatar_url: string;
+};
+
+type FormInfo = {
+  id: number | string;
+  title: string;
+  date_created: string;
+  registers: number | string;
+  user_created: number | string;
+};
+
+interface EntryType {
+  id: string | number;
+  form_id: string | number;
+  date_created: string;
+  created_by: number;
+  author_info: AuthorInfo | null;
+  form_info: FormInfo;
+}
+
+export type { EntryType };
