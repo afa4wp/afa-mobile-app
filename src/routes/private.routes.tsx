3a264aec1f43e00daa6e53e1e89622849bbbdb5d
claudionhangapc <claudionhangapc@gmail.com>
2023-06-28 22:39:46 -0300

    changed FormProvider

--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -4,36 +4,38 @@ import { ProfileScreen } from '../screens/private/profile/ProfileScreen';
 import { AboutScreen } from '../screens/private/about/AboutScreen';
 import { HomeTabs } from './private/HomeTabs';
 import LanguageContext from '../context/LanguageContext';
-
+import FormProvider from '../providers/FormProvider';
 const Stack = createNativeStackNavigator();
 
 export default function PrivateRoutes() {
   const { i18n } = useContext(LanguageContext)!;
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerTitleStyle: {
-          color: '#383042',
-        },
-      }}
-    >
-      <Stack.Screen
-        name="Home"
-        component={HomeTabs}
-        options={{
-          headerShown: false,
+    <FormProvider>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleStyle: {
+            color: '#383042',
+          },
         }}
-      />
-      <Stack.Screen
-        name="About"
-        component={AboutScreen}
-        options={{ title: i18n.t('screen.headTitle.about') }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ title: i18n.t('screen.headTitle.profile') }}
-      />
-    </Stack.Navigator>
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeTabs}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          options={{ title: i18n.t('screen.headTitle.about') }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: i18n.t('screen.headTitle.profile') }}
+        />
+      </Stack.Navigator>
+    </FormProvider>
   );
 }
