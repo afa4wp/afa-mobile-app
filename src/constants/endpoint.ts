fe51cc6aaef027d5768bd54044b8def93811780d
claudionhangapc <claudionhangapc@gmail.com>
2023-12-02 23:30:38 -0300

    changed API_NAMESPACE

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,4 +1,4 @@
 const REFRESH_TOKEN = '/user/tokens/refresh';
-const API_NAMESPACE = '/wp-all-forms-api/v1';
+const API_NAMESPACE = '/afa-submission-manager/v1';
 const WP_REST = '/wp-json';
 export { REFRESH_TOKEN, API_NAMESPACE, WP_REST };
